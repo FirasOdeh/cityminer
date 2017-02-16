5522a76cfd3b3799f0d3cd6e544d452b94c057f6
zekri <koussilazekri@gmail.com>
2017-02-16 12:27:44 +0100

    correction bug

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,10 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'defaultcontroller';
-$route['login'] = 'user_authentication/login';
-$route['register'] = 'user_authentication/register';
-$route['logout'] = 'user_authentication/logout';
-$route['(:any)'] = 'welcome/$1';
+$route['default_controller'] = 'DefaultController';
+$route['login'] = 'UserAuthentication/login';
+$route['register'] = 'UserAuthentication/register';
+$route['logout'] = 'UserAuthentication/logout';
+$route['(:any)'] = 'Welcome/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/controllers/UserAuthentication.php
+++ application/controllers/UserAuthentication.php
@@ -2,7 +2,7 @@
 
 //session_start(); //we need to start session in order to access it through CI
 
-Class User_Authentication extends CI_Controller {
+Class UserAuthentication extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
